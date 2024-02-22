6e4d0d52c1d439ce3fb20e88e257dac2253cc581
ayoubzann <ayoubzann@gmail.com>
2024-02-22 13:29:51 +0100

    Added card components

--- Studiesprang/src/pages/Home.jsx
+++ Studiesprang/src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Heading, Card, CardHeader, CardBody, Text } from "@chakra-ui/react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="p-10">
+      <Heading>Hva trenger du hjelp til?</Heading>
+      <div className="flex gap-15">
+      <Card>
+        <CardHeader><h2 className="text-xl font-extrabold">AI-Sensor</h2></CardHeader>
+        <CardBody>
+          <Text><p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, placeat!</p></Text>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardHeader><h2 className="text-xl font-extrabold">Studieteknikk</h2></CardHeader>
+        <CardBody>
+          <Text><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, est?</p></Text>
+        </CardBody>
+      </Card>
+      </div>
+     
+    </div>
+  );
+};
 
-export default Home
+export default Home;
