234306d11da0af7150a005686f1513822e9d6365
ayoubzann <ayoubzann@gmail.com>
2024-02-22 19:15:37 +0100

    Added footer

--- Studiesprang/src/App.jsx
+++ Studiesprang/src/App.jsx
@@ -1,22 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
-import Navbar from './components/Navbar/Navbar';
+import Navbar from "./components/Navbar/Navbar";
 import Login from "./pages/Login";
 import AiSensor from "./pages/AiSensor";
-import Studieteknikker from "./pages/Studieteknikker";
+import Studieteknikker from "./pages/Studieteknikker/Studieteknikker";
+import FooterElement from "./components/Footer/FooterElement";
 
 function App() {
   return (
-    <div className=" reviewcard flex flex-col ">
+    <div className="app-container">
       <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/studieteknikker" element={<Studieteknikker />} />
-          <Route path="/aisensor" element={<AiSensor />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <Navbar />
+        <div className="main-content flex justify-center">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/studieteknikker" element={<Studieteknikker />} />
+            <Route path="/aisensor" element={<AiSensor />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <FooterElement />
       </BrowserRouter>
     </div>
   );
