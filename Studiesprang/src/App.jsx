4cd0554dab1f8f1d8b77c2932e22752ca654f72c
ayoubzann <ayoubzann@gmail.com>
2024-02-22 13:10:58 +0100

    Added route for AiSensor

--- Studiesprang/src/App.jsx
+++ Studiesprang/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Navbar from './components/Navbar/Navbar';
 import Login from "./pages/Login";
+import AiSensor from "./pages/AiSensor";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/aisensor" element={<AiSensor />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
