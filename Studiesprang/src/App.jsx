942501e9656eefa4fd8d35247bbda8da12a3cbdb
ayoubzann <ayoubzann@gmail.com>
2024-02-22 12:58:47 +0100

    Added div

--- Studiesprang/src/App.jsx
+++ Studiesprang/src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col ">
       <BrowserRouter>
       <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
