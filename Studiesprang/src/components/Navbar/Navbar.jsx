f2f45c755ca1afd8ac43e25bcf47abc89daa665f
ayoubzann <ayoubzann@gmail.com>
2024-02-22 13:10:04 +0100

    Added link to AiSensor

--- Studiesprang/src/components/Navbar/Navbar.jsx
+++ Studiesprang/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,21 @@
-import TekstLogo from "../../assets/LogoBilde.png"
-import { Link } from 'react-router-dom'
+import TekstLogo from "../../assets/LogoBilde.png";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar sticky">
-      <img src={TekstLogo} alt="Studiesprang Logo" className='nav-logo'/>
+      <img src={TekstLogo} alt="Studiesprang Logo" className="nav-logo" />
       <div className="nav-links">
-      <Link to="/"><p className="text-xl">Studieteknikker</p></Link>
-      <Link to="/login"><p className="text-xl">AI-Sensor</p></Link>
+        <Link to="/">
+          <p className="text-xl">Studieteknikker</p>
+        </Link>
+        <Link to="/aisensor">
+          <p className="text-xl">AI-Sensor</p>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
