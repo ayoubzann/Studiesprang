c3408da4787c467009b5285b2a4c9b9e95a3d469
ayoubzann <ayoubzann@gmail.com>
2024-02-22 12:29:47 +0100

    Added pagination

--- Studiesprang/src/App.jsx
+++ Studiesprang/src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <>
-    Hei
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
