ef13b5f77c9991871ebb67ead1670fb3955d8e26
ayoubzann <ayoubzann@gmail.com>
2024-02-22 16:44:39 +0100

    Refactor

--- Studiesprang/src/pages/Home/Home.jsx
+++ Studiesprang/src/pages/Home/Home.jsx
@@ -3,19 +3,20 @@ import AiSensorImg from "../../assets/ai.png";
 import StudieteknikkImg from "../../assets/study.png";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import ReviewCard from "../../components/Navbar/Reviewcard/ReviewCard";
 
 const Home = () => {
   return (
     <div className="p-10">
       <Heading className="p-5">Hva trenger du hjelp til?</Heading>
       <div className="flex flex-wrap gap-10 justify-center">
-        <Link className="shadow-xl" to="/aisensor">
+        <Link className="linkcard shadow-lg" to="/aisensor">
           <Card className="p-2">
             <CardBody>
               <img
                 className="linkimg"
                 src={AiSensorImg}
-                alt="technology icons"
+                alt="Link ikon til AI-sensorsiden"
               />
             </CardBody>
             <CardHeader>
@@ -23,16 +24,23 @@ const Home = () => {
             </CardHeader>
           </Card>
         </Link>
-        <Link className="shadow-xl" to="/studieteknikker">
+        <Link className="linkcard shadow-lg" to="/studieteknikker">
           <Card className="p-2">
             <CardBody>
-              <img className="linkimg" src={StudieteknikkImg} alt="AI-Sensor" />
+              <img
+                className="linkimg"
+                src={StudieteknikkImg}
+                alt="Link ikon til studieteknikker-siden"
+              />
             </CardBody>
             <CardHeader>
               <h2 className="text-xl font-extrabold">Studieteknikk</h2>
             </CardHeader>
           </Card>
         </Link>
+      </div>
+      <div className="reviews">
+        <ReviewCard/>
       </div>
     </div>
   );
