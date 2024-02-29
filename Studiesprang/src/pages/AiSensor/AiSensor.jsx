aaa5aebe0fb5a0a35b05f3c77adffd41063ba8ba
ayoubzann <ayoubzann@gmail.com>
2024-02-29 14:12:34 +0100

    Moved file

---
+++ Studiesprang/src/pages/AiSensor/AiSensor.jsx
@@ -0,0 +1,19 @@
+
+import { Heading } from '@chakra-ui/react';
+const AiSensor = () => {
+
+  return (
+    <div>
+      <header className='m-5'>
+      <h1 className="text-5xl font-extrabold text-slate-300">AI Sensor</h1>
+      </header>
+      <div className='aisensor-header flex gap-2 m'>
+      <p className='text-5xl'>👇</p>
+      <Heading>Slik går du frem med AI-Sensoren</Heading>
+      </div>
+
+    </div>
+  )
+}
+
+export default AiSensor
