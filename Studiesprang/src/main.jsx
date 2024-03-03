308eaea3bb864f1f5ae305c88c2bed1519905543
ayoubzann <ayoubzann@gmail.com>
2024-03-03 15:48:21 +0100

    Added ClerkProvider

--- Studiesprang/src/main.jsx
+++ Studiesprang/src/main.jsx
@@ -3,11 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from './components/Navbar/Navbar';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Clerk PK not found.");
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <ChakraProvider>
+          <Navbar/>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>
 );
