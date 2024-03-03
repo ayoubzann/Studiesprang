21a7b000a4b7d19fa0265eba81c3c26600cc5fe5
ayoubzann <ayoubzann@gmail.com>
2024-03-03 15:48:14 +0100

    Updated route structure

--- Studiesprang/src/App.jsx
+++ Studiesprang/src/App.jsx
@@ -1,28 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
 import Login from "./pages/Login";
 import AiSensor from "./pages/AiSensor/AiSensor";
 import Studieteknikker from "./pages/Studieteknikker/Studieteknikker";
-import FooterElement from "./components/Footer/FooterElement";
+import { SignedOut } from "@clerk/clerk-react";
+import { SignedIn } from "@clerk/clerk-react";
+import React from "react";
 
 function App() {
   return (
-    <div className="app-container">
-      <BrowserRouter>
-        <Navbar />
-        <div className="main-content flex justify-center">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/studieteknikker" element={<Studieteknikker />} />
-            <Route path="/aisensor" element={<AiSensor />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-        <FooterElement />
-      </BrowserRouter>
-    </div>
+    <Routes className="app-container">
+      <Route
+        className="main-content flex justify-center"
+        path="/*"
+        element={
+          <React.Fragment>
+            <SignedOut>
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </SignedOut>
+            <SignedIn>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/studieteknikker" element={<Studieteknikker />} />
+                <Route path="/aisensor" element={<AiSensor />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </SignedIn>
+          </React.Fragment>
+        }
+      />
+    </Routes>
   );
 }
 
