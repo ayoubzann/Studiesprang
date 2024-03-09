1ebefa9f6a0b9feefb79c450caf0a5476e0ef9e0
ayoubzann <ayoubzann@gmail.com>
2024-03-09 15:27:14 +0100

    Vercel edits

--- Studiesprang/src/components/AiSensor/SSlideB.jsx
+++ Studiesprang/src/components/AiSensor/SSlideB.jsx
@@ -1,26 +1,6 @@
-import React, { useState } from "react";
-import { SignIn, SignedOut } from "@clerk/clerk-react";
-import { SignedIn } from "@clerk/clerk-react";
-import { Document, Page, pdfjs } from "react-pdf"; // Correct import
-import { Worker } from "@react-pdf-viewer/core"; // Import Worker from @react-pdf-viewer/core
 import "./styles/SensorcardA.css";
 
-const SSlideA = ({ criteria, handleFileUpload, setCurrentPage }) => {
-  // const [criteria, setCriteria] = useState("");
-  const [file, setFile] = useState(null);
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState();
-
-  const onFileChange = (event) => {
-    const uploadedFile = event.target.files[0];
-    setFile(uploadedFile);
-
-    pdfjs
-      .getDocument(URL.createObjectURL(uploadedFile))
-      .promise.then((pdfDocument) => {
-        setNumPages(pdfDocument.numPages);
-      });
-  };
+const SSlideA = ({  handleFileUpload, setCurrentPage }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
